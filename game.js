9d88fa341e1f56d276c5958518e4191bdf91fc27
gioin <injgia15@gmail.com>
2021-02-02 19:15:09 +0400

    completed version

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptionAnswer = false;
@@ -7,32 +12,42 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "How old are you?",
-    choice1: "14",
-    choice2: "19",
-    choice3: "15",
-    choice4: "30",
-    answer: 1,
-  },
-  {
-    question: "What is your favorite food?",
-    choice1: "Pizza",
-    choice2: "Khinkali",
-    choice3: "Khachapuri",
-    choice4: "Tolma",
-    answer: 2,
-  },
-  {
-    question: "What is your favorite song?",
-    choice1: "Country rouds",
-    choice2: "Yesterday",
-    choice3: "Don't let me down",
-    choice4: "High hopes",
-    answer: 3,
-  },
-];
+let questions = [];
+
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple"
+)
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formatetQuestions = {
+        question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formatetQuestions.answer = Math.floor(Math.random() * 3 + 1);
+      answerChoices.splice(
+        formatetQuestions.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formatetQuestions["choice" + (index + 1)] = choice;
+      });
+      return formatetQuestions;
+    });
+
+    //questions = loadedQuestions;
+    startGame();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -42,17 +57,21 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
   getNewQuestion();
 };
 
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
     //enter end page
     return window.location.assign("/end.html");
   }
 
   questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //update the progressbar
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -71,12 +90,14 @@ choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     if (!acceptionAnswer) return;
 
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
     acceptionAnswer = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
     let classToApply = "incorect";
     if (currentQuestion.answer == selectedAnswer) {
       classToApply = "correct";
+      incrementScoure(CORRECT_BONUS);
     }
 
     selectedChoice.classList.add(classToApply);
@@ -87,8 +108,10 @@ choices.forEach((choice) => {
     }, 1000);
 
     console.log(selectedChoice);
-
   });
 });
 
-startGame();
+incrementScoure = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
